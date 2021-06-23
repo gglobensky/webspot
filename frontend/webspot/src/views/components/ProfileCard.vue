7fe419b702f67299136e8981f820ac5069ca7b2a
Guillaume <gglobensky@gmail.com>
2021-06-23 02:42:39 -0400

    Improved look and responsiveness of Follow Search page. Added ability to see tags on the page too

--- frontend/webspot/src/views/components/ProfileCard.vue
+++ frontend/webspot/src/views/components/ProfileCard.vue
@@ -1,31 +1,27 @@
 <template>
-  <div class="card-panel grey lighten-5 z-depth-1 h-100">
-    <div class="col col-12">
-        <div class="row">
-            <div class="col col-12 d-flex justify-content-center" style="height: 128px;">
-                <slot name="image" />
-            </div>
-        </div>
-        <div class="row nopadding">
-            <div class="col col-12 d-flex justify-content-center">
-                <h1 class="h5">
-                    <slot name="header" />
-                </h1>
-            </div>
-
-                <div class="col col-12 mt-3 d-flex justify-content-center" style="height: 16vh;">
-                    <p class="text-center text-overflow-ellipsis">
-                        <slot name="body" />
-                    </p>
+    <div class="card-panel grey lighten-5 z-depth-1 d-flex" style="margin-bottom: 0px">
+        <div class="col col-12">
+            <div class="row" style="height: 15vh">
+                <div class="col col-12 d-flex justify-content-center h-100">
+                    <slot name="image" />
                 </div>
+            </div>
+            <div class="row nopadding">
+                <div class="col col-12 d-flex justify-content-center">
+                    <h1 class="h5">
+                        <slot name="header" />
+                    </h1>
+                </div>
+                
+                <slot name="body" />
 
-            <div class="col col-12 d-flex justify-content-end nopadding">
-                <a @click="hidePeople()" class="btn-floating waves-effect waves-light me-3 mb-3 default-pale"><i class="material-icons">remove</i></a>
-                <a @click="addPeople()" class="btn-floating waves-effect waves-light default-dark"><i class="material-icons">add</i></a>
+                <div class="col col-12 d-flex justify-content-end nopadding">
+                    <a @click="hidePeople()" class="btn-floating waves-effect waves-light me-3 mt-3 default-pale"><i class="material-icons">remove</i></a>
+                    <a @click="addPeople()" class="btn-floating waves-effect waves-light mt-3 default-dark"><i class="material-icons">add</i></a>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <script>
--- frontend/webspot/src/views/pages/FollowSearch.vue
+++ frontend/webspot/src/views/pages/FollowSearch.vue
@@ -5,19 +5,31 @@
     <div class="col col-sm-5 col-md-4 col-lg-3 fixed-top hidden-on-xs mt-4 z-2000">
         <form @submit.prevent="searchPeople">
             <div class="card-panel grey lighten-5 z-depth-1" style="height: 87vh; position: relative; top: 62px;">
-                <form-field id="'username'" v-model="searchData.searchTerms" label="Search" />
+                <div class="scrollable" style="height: 90%">
+                <form-field id="'username'" v-model="searchData.searchTerms" label="Search"/>
+                <h6>{{$t('in_group')}}</h6>
                 <div class="input-field mb-5">
-                    <select v-model="searchData.searchFor">
-                        <option :disabled="group == searchForTerms[0]" :selected="group == searchForTerms[0]" v-for="group in searchForTerms" :key="group" :value="group">{{$t(group)}}</option>
-                    </select>
+                    <div style="height: 15vh">
+                        <label v-for="group in searchForTerms" :key="group" :value="group" class="col col-12">
+                            <input name="group" type="radio" v-model="searchData.searchFor" :value="group">
+                            <span>{{$t(group)}}</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="mt-5">
+                <h6>{{$t('by_term')}}</h6>
                 </div>
                 <div class="input-field mb-5">
-                    <select v-model="searchData.searchBy">
-                        <option :disabled="term == searchByTerms[0]" :selected="term == searchByTerms[0]" v-for="term in searchByTerms" :key="term" :value="term">{{$t(term)}}</option>
-                    </select>
+                    <p>
+                        <label v-for="term in searchByTerms" :key="term" :value="term" class="col col-12">
+                            <input name="term" type="radio" v-model="searchData.searchBy" :value="term">
+                            <span>{{$t(term)}}</span>
+                        </label>
+                    </p>
                 </div>
                 <div class="input-field">
-                    <button class="waves-effect waves-light btn right">{{$t('search')}}</button>
+                    <button class="waves-effect waves-light btn right mt-5">{{$t('search')}}</button>
+                </div>
                 </div>
             </div>
         </form>
@@ -29,17 +41,35 @@
         </div>
         <div class="col col-12 col-sm-7 col-md-8 col-lg-9 nopadding">     
             
-            <div v-for="(n, i) in displayedPeopleData.length" :key="i" class="col col-12 col-sm-12 col-md-6 col-lg-4 vh-50 mb-1 pe-3">
-                <profile-card @hidePeople="hidePeople(displayedPeopleData[i].id, displayedPeopleData[i].username)" @addPeople="addPeople(displayedPeopleData[i].id, displayedPeopleData[i].username)" imageSrc="https://randomuser.me/api/portraits/women/28.jpg">
+            <div v-for="(n, i) in displayedPeopleData.length" :key="i" class="col col-12 col-sm-12 col-md-6 col-lg-4 pe-3">
+                <profile-card @hidePeople="hidePeople(displayedPeopleData[i].id, displayedPeopleData[i].username)" @addPeople="addPeople(displayedPeopleData[i].id, displayedPeopleData[i].username)" >
                     <template #image>
-                        <img v-if="displayedPeopleData[i].url" :src="`http://localhost:3000/${displayedPeopleData[i].url}`" :alt="`${displayedPeopleData[i].username}'s profile picture`" class="circle " style="max-height:128px; max-width: 128px" />
+                        <img v-if="displayedPeopleData[i].url" :src="`${API_URL}${displayedPeopleData[i].url}`" :alt="`${displayedPeopleData[i].username}'s profile picture`" class="circle " style="max-height:128px; max-width: 128px" />
                         <img v-else :src="require('@/assets/images/user.png')" :alt="`${displayedPeopleData[i].username}'s profile picture`" class="circle " style="max-height:128px; max-width: 128px" />
                     </template>
                     <template #header>
                         {{displayedPeopleData[i].username}}
                     </template>
                     <template #body>
-                        {{displayedPeopleData[i].bio}}
+                        <div v-if="true">
+                            <div class="col col-12 mt-2 d-flex justify-content-center" style="height: 16vh;">
+                                <p class="text-center text-overflow-ellipsis-5l">
+                                    {{displayedPeopleData[i].bio}}
+                                </p>
+                            </div>
+                        </div>
+                        <div v-else>
+                            <div class="col col-12 mt-2 d-flex justify-content-start" style="height: 8vh;">
+                                <p class="text-center text-overflow-ellipsis-2l">
+                                    Interests: placeholder placeholder placeholder placeholder placeholder placeholder placeholder placeholder
+                                </p>
+                            </div>               
+                            <div class="col col-12 d-flex justify-content-start" style="height: 8vh;">
+                                <p class="text-center text-overflow-ellipsis-2l">
+                                    Talents: placeholder placeholder placeholder placeholder placeholder placeholder placeholder placeholder
+                                </p>
+                            </div>    
+                        </div>        
                     </template>
                 </profile-card>
             </div>
@@ -50,6 +80,7 @@
 
 <script>
 import { securedAxiosInstance } from '../../backend/axios'
+import { API_URL } from '../../helper/serverRequests'
 import ProfileCard from '../components/ProfileCard.vue'
 import FormField from '../components/FormField.vue'
 import Toast from '../components/Toast.vue'
@@ -66,14 +97,12 @@ export default {
     setup(){
         const { t } = useI18n()
         const searchForTerms = [
-            "in_group",
             "new_people",
             "hidden_people",
             "hidden_followed",
             "followed"
         ]
         const searchByTerms = [
-            "by_term",
             "username",
             "bio"
         ]
@@ -122,12 +151,6 @@ export default {
         }
         function searchPeople(){
             //0 is the placeholder, a disabled option
-            if (searchData.searchFor == searchForTerms[0])
-                searchData.searchFor = searchForTerms[1]
-
-            if (searchData.searchBy == searchByTerms[0])
-                searchData.searchBy = searchByTerms[1]
-
             securedAxiosInstance.post('/following/search', { searchBy: searchData.searchBy, searchFor: searchData.searchFor, searchTerms: searchData.searchTerms? searchData.searchTerms.toLowerCase() : "" })
                 .then(response => {
                     displayedPeopleData.value = response.data
@@ -154,7 +177,7 @@ export default {
             console.log("Dont forget infinite scroll " + e)
         }
 
-        return { toastHtmlContent, toastRef, displayedPeopleData, searchByTerms, searchForTerms, addPeople, hidePeople, searchPeople, searchData }
+        return { API_URL, toastHtmlContent, toastRef, displayedPeopleData, searchByTerms, searchForTerms, addPeople, hidePeople, searchPeople, searchData }
     }
 
 }
